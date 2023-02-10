685d72b3bb74831357efc6ce3149c1653ac97604
vinh.le6 <vinh.laq.cntt@gmail.com>
2023-02-10 14:04:55 +0700

    revert train_2.py

--- pix2tex/train_2.py
+++ pix2tex/train_2.py
@@ -14,20 +14,6 @@ from pix2tex.models import get_model
 # from pix2tex.utils import *
 from pix2tex.utils import in_model_path, parse_args, seed_everything, get_optimizer, get_scheduler, gpu_memory_check
 
-def validation_testing(args, dataloader, e = None):
-    model = get_model(args)
-    model.load_state_dict(torch.load(os.path.join(args.model_path, args.name, "test.pth")))
-    
-    if e is not None:
-        num_batches = args.valbatches
-        name = 'val'
-    else:
-        num_batches = args.testbatchsize
-        name = 'test'
-
-    with torch.no_grad():
-        bleu_score, edit_distance, token_accuracy = evaluate(model, dataloader, args, num_batches=num_batches, name=name)
-
 
 def train(args):
     dataloader = Im2LatexDataset().load(args.data)
@@ -45,20 +31,20 @@ def train(args):
     model = get_model(args)
     if torch.cuda.is_available() and not args.no_cuda:
         gpu_memory_check(model, args)
-    # val_max_bleu, val_max_token_acc = 0, 0
-    # test_max_bleu, test_max_token_acc = 0, 0
+    val_max_bleu, val_max_token_acc = 0, 0
+    test_max_bleu, test_max_token_acc = 0, 0
     out_path = os.path.join(args.model_path, args.name)
     os.makedirs(out_path, exist_ok=True)
 
     if args.load_chkpt is not None:
         model.load_state_dict(torch.load(args.load_chkpt, map_location=device))
 
-    def save_models():
-        # if test:
-        #     filename = os.path.join(out_path, '%s_e%02d_step%02d_test.pth' % (args.name, e+1, step))
-        # else:
-        #     filename = os.path.join(out_path, '%s_e%02d_step%02d.pth' % (args.name, e+1, step))
-        filename = os.path.join(out_path, "test.pth")
+    def save_models(e, step=0, test = False):
+        if test:
+            filename = os.path.join(out_path, '%s_e%02d_step%02d_test.pth' % (args.name, e+1, step))
+        else:
+            filename = os.path.join(out_path, '%s_e%02d_step%02d.pth' % (args.name, e+1, step))
+        
         # print("Name: ", filename)
         # print("Model: ", model.state_dict())
         torch.save(model.state_dict(), filename)
@@ -75,14 +61,13 @@ def train(args):
     if args.scheduler == 'StepLR':
         scheduler = get_scheduler(args.scheduler)(opt, step_size=args.lr_step, gamma=args.gamma)
     elif args.scheduler == 'OneCycleLR':
-        scheduler = get_scheduler(args.scheduler)(opt, max_lr=args.max_lr, pct_start=args.pct_start, total_steps=round(int(len(dataloader)*args.epochs)))
+        scheduler = get_scheduler(args.scheduler)(opt, max_lr=args.lr, pct_start=args.pct_start, total_steps=round(int(len(dataloader)*args.epochs)))
 
     microbatch = args.get('micro_batchsize', -1)
     if microbatch == -1:
         microbatch = args.batchsize
 
     test_counter = 0
-    valid_counter = 0
 
     try:
         for e in range(args.epoch, args.epochs):
@@ -92,67 +77,59 @@ def train(args):
             #resets the memory allocation tracker at the beginning of each epoch
             torch.cuda.reset_max_memory_allocated()
 
-            # for i, (seq, im) in enumerate(dset):    
-                # if seq is not None and im is not None:
-                #     opt.zero_grad()
-                #     total_loss = 0
-                #     for j in range(0, len(im), microbatch):
-                #         tgt_seq, tgt_mask = seq['input_ids'][j:j+microbatch].to(device), seq['attention_mask'][j:j+microbatch].bool().to(device)
-                #         loss = model.data_parallel(im[j:j+microbatch].to(device), device_ids=args.gpu_devices, tgt_seq=tgt_seq, mask=tgt_mask)*microbatch/args.batchsize
-                #         loss.backward()  # data parallism loss is a vector
-                #         total_loss += loss.item()
-                #         torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-                #     opt.step()
-                #     scheduler.step()
-                #     dset.set_description('Loss: %.4f' % total_loss)
-                #     if args.wandb:
-                #         wandb.log({'train/loss': total_loss})
-                #         wandb.log({'train/lr': scheduler.get_last_lr()[0]})
+            for i, (seq, im) in enumerate(dset):    
+                if seq is not None and im is not None:
+                    opt.zero_grad()
+                    total_loss = 0
+                    for j in range(0, len(im), microbatch):
+                        tgt_seq, tgt_mask = seq['input_ids'][j:j+microbatch].to(device), seq['attention_mask'][j:j+microbatch].bool().to(device)
+                        loss = model.data_parallel(im[j:j+microbatch].to(device), device_ids=args.gpu_devices, tgt_seq=tgt_seq, mask=tgt_mask)*microbatch/args.batchsize
+                        loss.backward()  # data parallism loss is a vector
+                        total_loss += loss.item()
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+                    opt.step()
+                    scheduler.step()
+                    dset.set_description('Loss: %.4f' % total_loss)
+                    if args.wandb:
+                        wandb.log({'train/loss': total_loss})
+                        wandb.log({'train/lr': scheduler.get_last_lr()[0]})
                     
-                #     #releases unoccupied memory at the end of each iteration
-                #     torch.cuda.empty_cache()  
+                    #releases unoccupied memory at the end of each iteration
+                    torch.cuda.empty_cache()    
 
-                #save model after every epoch            
-                # if (e+1) % args.save_freq == 0:
-
-                # if (i+1+len(dataloader)*e) % args.sample_freq == 0:
-            
+                if (i+1+len(dataloader)*e) % args.sample_freq == 0:
+                    #validation testing
+                    test_counter += 1
+                    with torch.no_grad():
+                        model.eval()
+                        bleu_score_val, edit_distance_val, token_accuracy_val = evaluate(model, valdataloader, args, num_batches=int(args.valbatches*e/args.epochs), name='val')
+                        if bleu_score_val > val_max_bleu and token_accuracy_val > val_max_token_acc:
+                            val_max_bleu, val_max_token_acc = bleu_score_val, token_accuracy_val
+                            save_models(e, step=i, test = False)
+                    model.train()
+                        
                 #test model on testing set each 5 times after validation test
-                
-                    # with torch.no_grad():
-                    #     model.eval()
-                    #     bleu_score_test, edit_distance_test, token_accuracy_test = evaluate(model, testloader, args, num_batches=args.testbatchsize, name='test')
-                    #     if bleu_score_test > test_max_bleu and token_accuracy_test > test_max_token_acc:
-                    #         test_max_bleu, test_max_token_acc = bleu_score_test, token_accuracy_test
-                    #                                     save_models(e, step=i, test = True)  
-                    #     test_counter = 0
-                    # model.train()
-
-            #save model after every epochs
-            # save_models()
-            # call validation_testing function after every 2 epochs
-            if valid_counter == 1:
-                valid_counter = 0
-                test_counter += 1
-                print("----- Validation Testing -----")
-                validation_testing(args, valdataloader, e)
-            # else:
-            #     valid_counter += 1
-            
-            # #call validation_testing function with testloader, no epoch after every 5 epochs
-            if test_counter == 4:
-                test_counter = 0
-                print("----- Testing on Public Test set -----")
-                validation_testing(args, testloader)
-            
-            # if args.wandb:
-            #     wandb.log({'train/epoch': e+1})
+                if test_counter == 4 :
+                    with torch.no_grad():
+                        model.eval()
+                        bleu_score_test, edit_distance_test, token_accuracy_test = evaluate(model, testloader, args, num_batches=args.testbatchsize, name='test')
+                        if bleu_score_test > test_max_bleu and token_accuracy_test > test_max_token_acc:
+                            test_max_bleu, test_max_token_acc = bleu_score_test, token_accuracy_test
+                            if args.wandb:
+                                wandb.log({'test_periodically/bleu': bleu_score_test, 'test_periodically/edit_distance': edit_distance_test, 'test_periodically/token_accuracy': token_accuracy_test})
+                            save_models(e, step=i, test = True)  
+                        test_counter = 0
+                    model.train()
+            #save model after every epoch            
+            if (e+1) % args.save_freq == 0:
+                save_models(e, step=len(dataloader), test = False)
+            if args.wandb:
+                wandb.log({'train/epoch': e+1})
     except KeyboardInterrupt:
-        pass
-    #     if e >= 2:
-    #         save_models()
-    #     raise KeyboardInterrupt
-    # save_models()
+        if e >= 2:
+            save_models(e, step=i)
+        raise KeyboardInterrupt
+    save_models(e, step=len(dataloader), test = False)
 
 
 if __name__ == '__main__':
